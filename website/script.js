454b66cf40e96dd2d57230dd1325217cda34d93a
Jihang Li <66895727+JihangLi1121@users.noreply.github.com>
2023-05-20 22:10:46 -0700

    created demo for google sign in

---
+++ website/script.js
@@ -0,0 +1,16 @@
+function handleCredentialResponse(response) {
+    // decodeJwtResponse() is a custom function defined by you
+    // to decode the credential response.
+    const responsePayload = decodeJwtResponse(response.credential);
+
+    console.log("ID: " + responsePayload.sub);
+    console.log('Full Name: ' + responsePayload.name);
+    console.log('Given Name: ' + responsePayload.given_name);
+    console.log('Family Name: ' + responsePayload.family_name);
+    console.log("Image URL: " + responsePayload.picture);
+    console.log("Email: " + responsePayload.email);
+}
+
+function decodeJwtResponse(response) {
+    // decoding of JWT Response
+}
