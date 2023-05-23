bb55447df3ede6da05209df7582c9b3718124152
Jason Chen <jasonchen6464@gmail.com>
2023-05-22 18:19:45 -0700

    initiated popup docs

--- extension/background.js
+++ extension/background.js
@@ -1,14 +1,33 @@
-function getword(info,tab) {
-  console.log("Word " + info.selectionText + " was clicked.");
-  chrome.tabs.create({  
-    url: "http://www.google.com/search?q=" + info.selectionText
+function injectTextbox(text) {
+  // Function to be injected
+  let injectionFunction = function(text) {
+      let textbox = document.createElement("textarea");
+      textbox.style.position = "fixed";
+      textbox.style.zIndex = 10000;
+      textbox.style.left = "20px";
+      textbox.style.top = "20px";
+      textbox.value = text;
+      document.body.appendChild(textbox);
+  };
+
+  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+      chrome.scripting.executeScript({
+          target: {tabId: tabs[0].id},
+          func: injectionFunction,
+          args: [text]
+      });
   });
+}
+
+function getword(info, tab) {
+  console.log("Word " + info.selectionText + " was clicked.");
+  injectTextbox(info.selectionText);
 }
 
 chrome.contextMenus.create({
   title: "Ask ChatGPT", 
   contexts:["selection"], 
-  id: "contextMenuId" // Add id here
+  id: "contextMenuId"
 }); 
 
 chrome.contextMenus.onClicked.addListener(getword);
---
+++ extension/popup_script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+
+
+    const btn = document.getElementById("loginBtn");
+    btn.addEventListener("click", () => {
+
+        chrome.tabs.create({  
+            url: "http://www.google.com/"
+        });
+    })
+}
