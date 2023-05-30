131aa32f8e1ddc8ac927bda7b1745235b6db1f76
Jihang Li <66895727+JihangLi1121@users.noreply.github.com>
2023-05-29 22:40:41 -0700

    adding post requests in backend and API calling

--- website/backend/script.js
+++ website/backend/script.js
@@ -77,11 +77,82 @@ app.post('/id/:inst', jsonParser, (req, res) => {
 });
 
 app.post('/login', jsonParser, (req, res) => {
-    
+
 });
 
-app.post('/:id/getResponse', jsonParser, (req, res) => {
+app.post('/id/getResponse', jsonParser, (req, res) => {
+    const body = req.body;
+    const id = body["ID"];
+    const text = body["text"];
+    let inst = "";
+    try {
+        const jsonData = fs.readFileSync("cred.json");
+        const userData = JSON.parse(jsonData);
+        for (i = 0; i < userData["users"].length; i++) {
+            if (userData["users"][i]["ID"] === id) {
+                inst = userData["users"][i]["Instruction Text"];
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+    const mesg = inst + " " + text;
 
+    // ChatGPT API call
+    // Define the API key
+    const API_KEY = process.env.OPENAI_API_KEY;
+
+    // Define the endpoint URL
+    const endpointUrl = "https://api.openai.com/v1/chat/completions";
+
+    // Define the headers
+    const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+    };
+
+    // Define the maximum number of completions to return
+    const maxCompletions = 1;
+
+    // Define the prompt to send to the API
+    const prompt = {
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: mesg}], // change prompt to messages array
+    temperature: 0.5,
+    max_tokens: 20,
+    top_p: 1,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+    stop: ["\n"],
+    };
+
+    // Send a POST request to the endpoint with the prompt and headers
+    fetch(endpointUrl, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(prompt), // prompt is an object, so no need to wrap it in another object.
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        // Log the response data to the console
+        console.log(data);
+
+        // Access the message object from the first choice in the response
+        let message = data.choices[0].message;
+        
+        // Access the role and content properties of the message object
+        let role = message.role;
+        let content = message.content;
+        
+        // Log the role and content to the console
+        console.log("Role: ", role);
+        console.log("Content: ", content);
+        res.send(content);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
 });
 
 app.listen(3000, () => {
