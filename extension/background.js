4f2ac98b3ac778cc4bea23d4a10cc0c961e95219
Jason Chen <jasonchen6464@gmail.com>
2023-05-31 18:39:58 -0700

    moved API call to extension background

--- extension/background.js
+++ extension/background.js
@@ -1,27 +1,35 @@
-function injectTextbox(text) {
-  // Function to be injected
-  let injectionFunction = function(text) {
+//const API_KEY = process.env.OPENAI_API_KEY; //TO BE MODIFIED LATER TO USER-BASED
+
+async function injectTextbox(API_KEY, text) {
+  //let response = await getResponse(API_KEY, text);
+  let response = await getResponse(API_KEY, text);
+  
+  // We'll be passing response as an argument
+  let injectionFunction = function(response) {
       let textbox = document.createElement("textarea");
       textbox.style.position = "fixed";
       textbox.style.zIndex = 10000;
       textbox.style.left = "20px";
       textbox.style.top = "20px";
-      textbox.value = text;
+      textbox.value = response;
       document.body.appendChild(textbox);
   };
 
   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
       chrome.scripting.executeScript({
           target: {tabId: tabs[0].id},
-          func: injectionFunction,
-          args: [text]
+          function: injectionFunction,
+          args: [response]  // Pass response as an argument
       });
   });
 }
 
+
+
+
 function getword(info, tab) {
   console.log("Word " + info.selectionText + " was clicked.");
-  injectTextbox(info.selectionText);
+  injectTextbox("API_KEY", info.selectionText);
 }
 
 chrome.contextMenus.create({
@@ -31,3 +39,55 @@ chrome.contextMenus.create({
 }); 
 
 chrome.contextMenus.onClicked.addListener(getword);
+
+
+//------------------------------------------------------------------------------------------------------//
+
+async function getResponse(API_KEY, text) {
+  // Define the endpoint URL
+  const endpointUrl = "https://api.openai.com/v1/chat/completions";
+
+  // Define the headers
+  const headers = {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${API_KEY}`,
+  };
+
+  // Define the prompt to send to the API
+  const prompt = {
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: text}],
+    temperature: 0.5,
+    max_tokens: 150,
+    top_p: 1,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+    stop: ["\n"],
+  };
+
+  try {
+    let response = await fetch(endpointUrl, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(prompt),
+    })
+    let data = await response.json();
+    // Access the message object from the first choice in the response
+    let message = data.choices[0].message;
+  
+    // Access the role and content properties of the message object
+    let role = message.role;
+    let content = message.content;
+  
+    // Log the role and content to the console
+    console.log("Role: ", role);
+    console.log("Content: ", content);
+
+    // Return the content
+    return content;
+  } catch(error) {
+    console.error(error);
+  }
+  
+}
+
